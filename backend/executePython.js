99f0227116d4ee8d81e1529a2249771016fb8105
Jigyansu Nanda <62047889+jigyansunanda@users.noreply.github.com>
2023-01-13 09:14:47 +0530

    Update executePython.js

--- backend/executePython.js
+++ backend/executePython.js
@@ -2,13 +2,16 @@ const { exec } = require("child_process");
 // const fs = require("fs");
 // const path = require("path");
 
-const executePython = (filepath) => {
+const executePython = ([filepath, inputFilePath]) => {
     return new Promise((resolve, reject) => {
-        exec(`python3 ${filepath}`, (error, stdout, stderr) => {
-            if (error) reject({ error, stderr });
-            if (stderr) reject({ stderr });
-            resolve(stdout);
-        });
+        exec(
+            `python3 ${filepath} ${inputFilePath}`,
+            (error, stdout, stderr) => {
+                if (error) reject({ error, stderr });
+                if (stderr) reject({ stderr });
+                resolve(stdout);
+            }
+        );
     });
 };
 
